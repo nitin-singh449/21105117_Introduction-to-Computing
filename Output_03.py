5ba259c3e0ca2c4f12140ec0e193ee3cd76c979a
Nitin Singh <96660163+nitin-singh449@users.noreply.github.com>
2022-02-13 23:38:00 +0530

    Add files via upload

---
+++ Output_03.py
@@ -0,0 +1,86 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_01.py ==========
+Question 1
+Please give an input- nitin
+Total occurances
+n-2
+i-2
+t-1
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_01.py ==========
+Question 1
+Please give an input- hey whats up
+Total occurances
+hey-1
+whats-1
+up-1
+
+=========== RESTART: /Users/nitinsingh/Desktop/Python code/asg3_02.py ==========
+Question 2 
+Input a year: 2020
+Input a month [1-12]: 12
+Input a day [1-31]: 31
+The next date is [yyyy-mm-dd] 2021-1-1.
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_03.py ==========
+Question 3
+Enter the list: [1,2,3,4,5]
+Output: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_04.py ==========
+Question 4 
+Enter the grade point of the student: 9.7
+Your Grade is 'A' and Excellent Performance
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_q5.py ==========
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_q6.py ==========
+Question 6
+Please Enter SID of Student- 21105117
+Please enter the name of the Student- Nitin
+If you want to enter more details press Y/N- y
+Please Enter SID of Student- 21104120
+Please enter the name of the Student- Yogesh
+If you want to enter more details press Y/N- n
+Part a
+21105117 is Nitin
+21104120 is Yogesh
+
+Part b
+value sorted dictionary is ['Nitin', 'Yogesh']
+
+Part c
+Keys sorted dictionary is [21104120, 21105117]
+
+Part d
+Please enter the student's SID whose detail you need- 21105117
+Name of the required student is Nitin
+
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_q7.py ==========
+Enter the no. of terms : 4
+The Fibonacci sequence is as follows:
+0 1 1 2 
+The average of the terms of Fibonacci sequence upto 4 terms is 1.00
+
+========== RESTART: /Users/nitinsingh/Desktop/Python code/asg03_q8.py ==========
+Question 8 
+Part a
+Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+Part b
+Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+Part c
+Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+Part d
+Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+Part e
+Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
